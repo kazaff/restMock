fad0617f05ea48df0b775378c8c4ee67f8114d32
kazaff <edisondik@gmail.com>
2014-04-17 18:01:17 +0800

    init

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by @kazaff on 2014/4/17.
+ */
+"use strict";
+
+module.exports = {
+    target: "./server"
+    , entry: "./server/app.js"
+};
---
+++ watch.js
@@ -0,0 +1,19 @@
+/**
+ * Created by @kazaff on 2014/4/17.
+ */
+"use strict";
+var FS = require("fs");
+var Config = require("./config");
+var Cluster = require("cluster");
+Cluster.setupMaster({
+    exec: Config.entry
+    , silent: true
+});
+var worker = Cluster.fork();
+
+//监控指定目录
+FS.watch(Config.target,function(event, filename){
+    worker.kill();
+    worker = Cluster.fork();
+});
+
